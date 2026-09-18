760b0c3df0ac4b91933f942f63a4cd8ec10848be
santossebita0-sys <santossebita0@gmail.com>
2026-09-18 12:56:29 -0300

    Update main.lua

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,3 +1,14 @@
-function onStart()
-    print("¡Mi Primer Juego empezó!")
+local count = 0
+
+function OnStart()
+    local panel = EMRAPI.UI:CreatePanel(0.8, 0.5)
+    
+    local label = EMRAPI.UI:CreateText(panel, "Puntos: 0")
+    EMRAPI.UI:SetPosition(label, 0, 60, 0)
+    EMRAPI.UI:SetSize(label, 300, 50)
+
+    EMRAPI.UI:CreateButton(panel, "ATRAPAR", function()
+        count = count + 1
+        EMRAPI.UI:SetText(label, "Puntos: " .. count)
+    end)
 end
